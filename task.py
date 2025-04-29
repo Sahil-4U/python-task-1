a80858005710c155e2155a6cd575f172c77c02d4
sahil <sahil@befisc.com>
2025-04-29 23:25:01 +0530

    refactoring the code

--- task.py
+++ task.py
@@ -1,5 +1,7 @@
 firstInput = int(input('Enter the first number: '))
 secondInput = int(input("Enter the second number: "))
+firstName = str(input('Enter your first name: '))
+lastName = str(input("Enter your last name: "))
 
 def addition(a,b):
     return a + b
@@ -13,17 +15,12 @@ def multiplication(a,b):
 def division(a,b):
     return a / b
 
-print('Addition: ',addition(firstInput,secondInput))
-print('Subtraction: ',subtraction(firstInput,secondInput))
-print ('Multiplication: ',multiplication(firstInput,secondInput))
-print('Division: ',division(firstInput,secondInput))
-
-firstName = str(input('Enter your first name: '))
-lastName = str(input("Enter your last name: "))
-
-
 def generate_gretting_message(firstname,lastname):
     return f'Hello , {firstname} {lastname}! Welcome to the Python program.'
 
 
+print('Addition: ',addition(firstInput,secondInput))
+print('Subtraction: ',subtraction(firstInput,secondInput))
+print ('Multiplication: ',multiplication(firstInput,secondInput))
+print('Division: ',division(firstInput,secondInput))
 print(generate_gretting_message(firstName,lastName))
