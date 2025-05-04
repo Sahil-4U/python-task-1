e081c4ee984fa4c411872af65f5e7e2cb362a03f
sahil <sahil@befisc.com>
2025-05-04 18:23:48 +0530

    updating the second task

---
+++ task2.py
@@ -0,0 +1,14 @@
+numberInput=int(input('Enter a number: '))
+
+if(numberInput % 2 == 0):
+    print(f'{numberInput} is an even number.')
+else:
+    print(f'{numberInput} is an odd number.')
+    
+sumOfIntegers=0
+
+for i in range(1,51):
+    sumOfIntegers=sumOfIntegers+i
+    
+    
+print(f'The sum of numbers from 1 to 50 is: ',sumOfIntegers)
